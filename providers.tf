38be21220ba156f95d441f0b08516a5a86ec3e3a
Jérôme Respaut <jerome.respaut@claranet.com>
2025-08-22 16:12:49 +0200

    chore(deps): 🔗 bump AzureRM provider version to v4.31+

--- providers.tf
+++ providers.tf
@@ -3,7 +3,7 @@ terraform {
     # tflint-ignore: terraform_unused_required_providers
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.18"
+      version = "~> 4.31"
     }
     # tflint-ignore: terraform_unused_required_providers
     azurecaf = {
