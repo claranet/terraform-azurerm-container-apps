41eca6f100b4f8415a57ee77d726ef0e5780919f
Jérôme Respaut <jerome.respaut@claranet.com>
2025-11-07 11:53:19 +0100

    chore: apply suggestions

--- modules/container-app-environment/r-container-apps-environment.tf
+++ modules/container-app-environment/r-container-apps-environment.tf
@@ -36,7 +36,7 @@ resource "azurerm_container_app_environment_certificate" "main" {
   certificate_blob_base64 = each.value.certificate_blob_base64
   certificate_password    = each.value.certificate_password
 
-  tags = merge(var.extra_tags, local.default_tags)
+  tags = merge(local.default_tags, var.extra_tags)
 }
 
 resource "azurerm_container_app_environment_custom_domain" "main" {
